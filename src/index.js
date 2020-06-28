d49c57f60a06e2634a1c6f833fbb2083e386711b
xuewenG <gexuewen@outlook.com>
2020-06-28 09:19:05 +0800

    feat(room): add-room

--- src/index.js
+++ src/index.js
@@ -6,44 +6,51 @@ expressWs(app);
 
 const clients = [];
 
-app.ws('/blog/sync-play/socket/:clientId', (ws, req) => {
+app.ws('/blog/sync-play/socket/:roomId/:clientId', (ws, req) => {
+    const roomId = req.params.roomId;
     const clientId = req.params.clientId;
-    console.log(`client connected: ${clientId}`);
+    console.log(`client connected: ${roomId}, ${clientId}`);
     clients.push({
-        clientId: clientId,
+        roomId,
+        clientId,
         socket: ws
     });
     sendCurrentCount();
-    ws.on('message', function (msg) {
+    ws.on('message', msg => {
         clients.forEach(client => {
             const socket = client.socket;
-            if (socket !== this) {
+            if (client.roomId === roomId && client.clientId !== clientId) {
                 socket.send(msg);
             }
         });
     });
-    ws.on('close', function (msg) {
-        console.log('client disconnected: ');
+    ws.on('close', msg => {
+        console.log(`client disconnected: ${roomId}, ${clientId}`);
         for (let i = 0; i < clients.length; i++) {
-            if (clients[i].socket === this) {
+            if (clients[i].clientId === clientId) {
                 clients.splice(i, 1);
+                break;
             }
         }
         sendCurrentCount();
     })
+
+    function sendCurrentCount() {
+        let count = 0;
+        const clientMarked = clients.filter(client => {
+            return client.roomId === roomId
+        });
+        clientMarked.forEach(client => {
+            const socket = client.socket;
+            socket.send(JSON.stringify({
+                event: 'count',
+                count: clientMarked.length
+            }));
+        });
+    }
 });
 
-function sendCurrentCount() {
-    clients.forEach(client => {
-        const socket = client.socket;
-        socket.send(JSON.stringify({
-            event: 'count',
-            count: clients.length
-        }));
-    });
-}
-
-let port = 80;
+const port = 80;
 
 app.listen(port);
 console.log(`Server running at: http://127.0.0.1:${port}`);
